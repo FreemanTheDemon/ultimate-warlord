12f2615006302676a051d39ffe5fb386a2b777ba
Freeman Bird <freemanbird@gmail.com>
2022-02-21 12:03:29 -0700

    units turned to objects

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from 'react';
+import './component-styles/game.css'
 import Tile from './Tile';
 import Unit from './Unit';
 import Battle from './Battle';
+import Details from './Details';
 
 const grid = [
     ['grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1', 'grass_1'],
@@ -20,12 +22,12 @@ const grid = [
 
 
 let units = [
-    ['', '', '', '', '', 'archer_1', '', '', '', '', '', ''],
+    ['', '', '', '', '', {units: [{id: 3, moving: false}, {id: 5, moving: true}, {id: 4, moving: false}, {id: 4, moving: true}], team: 2, x: 0, y: 5}, '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', 'wizard_1', '', '', '', '', '', '', ''],
+    ['', '', '', '', {units: [{id: 2, moving: false}], team: 0, x: 3, y: 4}, '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '', '', '', '', ''],
-    ['', '', '', '', '', '', '', '', '', '', '', ''],
+    ['', '', '', '', '', '', '', '', {units: [{id: 24, moving: false}], team: 1, x: 5, y: 8}, '', '', ''],
     ['', '', '', '', '', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '', '', '', '', ''],
     ['', '', '', '', '', '', '', '', '', '', '', ''],
@@ -36,19 +38,33 @@ let units = [
 
 /*
 unit ids:
-1 - Hero
-2 - Dragon
-3 - Wizard
-4 - Knight
-5 - Soldier
-6 - Archer
+
+0 - NAVIES - boat
+20+ - HEROES - lord
+1 - DRAGONS - dragon_green
+2 - WIZARDS - wizard
+3 - DEMONS - demon_red
+4 - DEVILS - helion
+5 - UNDEAD - skeleton_warrior/zombie_a with zombie_b's arm
+6 - PEGASI - valkyrie_a
+7 - GIANTS - ogre
+8 - GRIFFINS - spirit
+9 - DWARVES - dwarf
+10 - WOLF-RIDERS - wolf_black
+11 - CAVALRY - modified templar
+12 - HEAVY INFANTRY - knight/paladin
+13 - LIGHT INFANTRY - warrior
+14 - ELVEN ARCHERS - archer/elf_bowman
+15 - SCOUT - thief
 */
 
 // up to 8 units
-let unitObjExample = {units: ['wizard_1', 'archer_1', 'warrior_m_1', 'archer_1', 'archer_1', '', '', '']}
+// let unitsTable = [{bag: ['item1', 'item2'], id: 20}, {bag: ['item3', 'item4'], id: 21}];
+
+// let bags = [[]];
 
 function Game() {
-    const [selectedUnit, setSelectedUnit] = useState([null, null]); // [x, y]
+    const [selectedUnit, setSelectedUnit] = useState({id: null}); // {[units: [{id: NUM, moving: BOOL}], team: NUM, x: NUM, y: NUM}
     const [battle, setBattle] = useState(false);
     
     useEffect(() => {
@@ -62,31 +78,35 @@ function Game() {
         return () => {
             document.removeEventListener("keydown", listener);
         };
-      }, []);
+    }, []);
 
-    function selectUnit(x, y) {
-        setSelectedUnit([x, y]);
+    function selectUnit(unit) {
+        setSelectedUnit(unit);
     }
 
     function deselectUnit() {
-        setSelectedUnit([null, null]);
+        setSelectedUnit({id: null});
     }
 
     function moveUnit(x, y) {
-        if (selectedUnit[0] !== null) {
-            let unit = units[selectedUnit[0]][selectedUnit[1]];
-            units[selectedUnit[0]][selectedUnit[1]] = '';
-            setSelectedUnit([x, y]);
+        if (selectedUnit.id !== null) {
+            // copy the unit - change this to only copy only units with moving === true
+            let unit = Object.assign({}, units[selectedUnit.x][selectedUnit.y]);
+            // clear the unit - change this to instead delete unmoved unit data in that position
+            units[selectedUnit.x][selectedUnit.y] = '';
+            // set the new x and y values
+            unit.x = x;
+            unit.y = y;
+            // set the selected unit
+            // move the unit to the new position
+            setSelectedUnit(unit);
             units[x][y] = unit;
         }
     }
 
     return (
-        <div>
-            <p>
-                This is the Game.
-            </p>
-            {battle && <Battle />}
+        <div className="map-container">
+            {battle && <Battle attacker={null} defender={null} setBattle={setBattle}/>}
             <div className="tile-container">
                 {grid.map((item, x) => {
                     return (item.map((innerItem, y) => {
@@ -98,14 +118,21 @@ function Game() {
                 {units.map((item, x) => {
                     return (item.map((innerItem, y) => {
                         let isSelected = false;
-                        if (selectedUnit[0] === x && selectedUnit[1] === y) {
+                        if (selectedUnit.x === x && selectedUnit.y === y) {
                             isSelected = true;
                         }
-                        return (
-                            <Unit unit={`units/${innerItem}.png`} x={x} y={y} selectUnit={selectUnit} isSelected={isSelected} />
-                        );
+                        if (innerItem !== '') {
+                            return (
+                                <Unit unit={innerItem} x={x} y={y} selectUnit={selectUnit} isSelected={isSelected} />
+                            );
+                        } else {
+                            return undefined;
+                        }
                     }));
                 })}
+            </div>
+            <div className="details-container">
+                <Details />
             </div>
         </div>
     );
--- src/components/Unit.js
+++ src/components/Unit.js
@@ -2,17 +2,31 @@ import React from 'react';
 import './component-styles/tile.css';
 
 function Unit({unit, x, y, selectUnit, isSelected}) {
-    // selected: outline: '2px dashed yellow', outlineOffset: '-2px'
+    // {units: [{id: 21, moving: false}, {id: 2, moving: true}, {id: 2, moving: false}, {id: 4, moving: true}], team: 2, x: 2, y: 2}
+    const colors = ['red', 'yellow', 'blue'];
+    let unitColor = colors[unit.team];
+    let unitId = unit.units[0].id;
+    let unitNum = unit.units.length;
+    for (let i = 0; i < unit.units.length; i++) {
+        if (unit.units[i].id < unitId && unitId < 20) {
+            unitId = unit.units[i].id;
+        } else if (unit.units[i].id >= 20) {
+            unitId = 20;
+        }
+    }
+
     if (isSelected) {
         return (
             <div className="border-animation unit-container" style={{top: `calc(${x} * 48px)`, left: `calc(${y} * 48px)`}}>
-                <img onClick={()=>{selectUnit(x, y)}} className="unit" src={unit} alt='' />
+                <p className="unit-num" style={{color: unitColor}}>{unitNum}</p>
+                <img onClick={()=>{selectUnit(unit)}} className="unit" src={`units/${unitId}.gif`} alt='' />
             </div>
         );
     } else {
         return (
             <div className="unit-container" style={{top: `calc(${x} * 48px)`, left: `calc(${y} * 48px)`}}>
-                <img onClick={()=>{selectUnit(x, y)}} className="unit" src={unit} alt='' />
+                <p className="unit-num" style={{color: unitColor}}>{unitNum}</p>
+                <img onClick={()=>{selectUnit(unit)}} className="unit" src={`units/${unitId}.gif`} alt='' />
             </div>
         );
     }
